e70de3036622dbbf239e86c6c2b9467f70de72ce
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-04-23 16:31:02 +0530

    Add files via upload

---
+++ C operators/modulusoperator.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	printf("%d\n",1%2);
+	printf("%d\n",2%2);
+	printf("%d\n",3%2);
+	printf("%d\n",4%2);
+	printf("%d\n",5%2);
+	printf("%d",15%10);
+	printf("%d",150%10);
+	printf("%d",128%10);
+	
+	return 0;
+}
