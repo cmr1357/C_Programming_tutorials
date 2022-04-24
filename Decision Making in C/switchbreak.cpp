66a1b14054c30e5369953454c44808a74ce4ccb3
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-04-24 15:13:34 +0530

    Add files via upload

---
+++ Decision Making in C/switchbreak.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i=0;
+	switch(i)
+	{
+		case 1:printf("in 1\n");break;
+		case 2:printf("in 2\n");break;
+		case 3:printf("in 3\n");break;
+		case 4:printf("in 4\n");break;
+		case 5:printf("in 5\n");break;
+		default:printf("in default\n");break;
+		
+	}
+}
