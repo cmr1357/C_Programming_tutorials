34f02e19dce05fda669e532c9ca01b1429526dab
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-04-24 15:13:04 +0530

    Add files via upload

---
+++ Decision Making in C/Switch1example.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i=1;
+	switch(i)
+	{
+	
+	case 1:printf("I am in 1\n");
+	case 2:printf("I am in 2\n");
+	case 3:printf("I am in 3\n");
+	case 4:printf("I am in 4\n");
+	case 5:printf("I am in 5\n");
+	case 6:printf("I am in 6\n");
+	default :printf("In default state\n");
+	
+	}
+	return 0;
+}
