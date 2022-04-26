9081a49f75c215e8bda4849312524bd698eccbed
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-04-26 20:56:56 +0530

    Add files via upload

---
+++ C Loops/Nested while/Pattern2.c
@@ -0,0 +1,23 @@
+int main()
+{
+	int i,j,range;
+	printf("Enter the range for the pattern\n");
+	scanf("%d",&range);
+	printf("\n");
+	i=1;
+	while(i<=range)
+	{
+		j=1;
+		while(j<=i)
+		{
+			printf("*");
+			
+			j++;
+		}
+			printf("\n");
+			i++;
+	}
+	
+	
+	return 0;
+}
