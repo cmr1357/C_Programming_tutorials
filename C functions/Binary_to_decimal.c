45dd007db9b9c282bcc0ca1c40545aadc1b9b0f5
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-04-30 06:10:46 +0530

    Add files via upload

---
+++ C functions/Binary_to_decimal.c
@@ -0,0 +1,29 @@
+//DECIMAL TO BINARY METHOD-1
+#include<stdio.h>
+void bin(int);
+int main()
+{
+	int num=0;
+	int power=1;
+	printf("please enter a numeric value: ");
+	scanf("%d",&num);
+	printf("%d represented in binary is : ",num);
+	
+	decbin(num);
+	
+	return 0;
+}
+bin(int n)
+{	
+	int num=0;
+	int power=1;
+	while(power < num)
+	{
+		power *= 2;
+	}
+	if(num==0)
+	{
+		printf
+	}
+	return ;
+}
