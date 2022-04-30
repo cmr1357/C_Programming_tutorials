7ad766a04b462ac3afb03a0fd0f1635a2df96260
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-04-30 21:51:24 +0530

    Add files via upload

---
+++ C Files/Createfile.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+	FILE *fp;
+	//fp=fopen("hello.txt","w");//file is created in same location where your code is saved.
+	fp=fopen("Documents\\hello.txt","w");
+}
