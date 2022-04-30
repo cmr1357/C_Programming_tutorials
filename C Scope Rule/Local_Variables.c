1f53f14e2151d4b80d0f36ce18205077cff225db
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-04-30 06:15:46 +0530

    Add files via upload

---
+++ C Scope Rule/Local_Variables.c
@@ -0,0 +1,13 @@
+//Local Variables
+#include<stdio.h>
+int main()
+{
+	//local variable declaration
+	int a,b;
+	//a and b are local variable to the main function.
+	//initialization
+	a=10;
+	b=20;
+	printf("a= %d, b=%d\n",a,b);
+	return 0;
+}
