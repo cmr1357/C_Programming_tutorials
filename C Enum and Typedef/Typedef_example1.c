eef4402702929cb0e3e4e93f176f858a6371eda6
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-04-30 21:44:21 +0530

    Add files via upload

---
+++ C Enum and Typedef/Typedef_example1.c
@@ -0,0 +1,14 @@
+//typedef "rename a data type"
+/*typedef datatype New_name;
+example :
+typedef int number;
+int is replaced by number.
+*/
+#include<stdio.h>
+int main()
+{
+	typedef int num;
+	num i=10;
+	
+	printf("%d\n",i);
+}
