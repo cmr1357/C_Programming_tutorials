142596636b04cf4de95250ec621f7acfe682dc92
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-01 04:57:04 +0530

    Add files via upload

---
+++ C Data structures/Linked list/Traversing_linkedlist.c
@@ -0,0 +1,54 @@
+//Traversing linked list
+#include<stdio.h>
+#include<stdlib.h>
+
+
+	struct node
+	{
+		int data;
+		struct node *link;
+	};
+	void countnode(struct node *head);
+int main()
+{
+	
+	
+	struct node *head=NULL;
+	
+	head=(struct node*)malloc(sizeof(struct node));
+	head->data=45;
+	head->link=NULL;
+	
+	struct node *current=(struct node*)malloc(sizeof(struct node));
+	current->data=98;
+	current->link=NULL;
+	head->link=current;
+	
+	current=(struct node*)malloc(sizeof(struct node));
+	current->data=2;
+	current->link=NULL;
+	
+	head->link->link=current;
+	
+	
+	countnode(head);
+	
+	return 0;
+}
+void countnode(struct node *head)
+{
+	int count =0;
+	if(head==NULL)
+	{
+		printf("Linked list is empty\n");
+	}
+	struct node *ptr =NULL;
+	ptr=head;
+	while(ptr!=NULL)
+	{
+		count ++;
+		ptr=ptr->link;
+	}
+	printf("%d",count);
+		
+}
